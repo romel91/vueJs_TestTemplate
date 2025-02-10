2c4f917086ffba1981567a1fc305ced8a83057f0
romel <romelhasan741@gmail.com>
2025-02-11 01:05:50 +0600

    template customize started

---
+++ src/App copy 2.vue
@@ -0,0 +1,17 @@
+<script setup>
+import AlertComponent from './components/AlertComponent.vue';
+</script>
+
+<template>
+  <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+  <AlertComponent type="success" msg="This is a success alert" /><br>
+  <AlertComponent type="danger" msg="This is a danger alert" /><br>
+  <AlertComponent type="warning" msg="This is a warning alert" /><br>
+  <alert-component type="info" msg="This is a info alert"/>
+  <br>
+  <alert-component />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/HeaderSection.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+</script>
+<template>
+  <header class="p-4 pb-20">
+    <div class="w-full max-w-7xl mx-auto">
+      <div class="flex justify-between gap-8 items-center">
+        <div>
+          <a href="" class=" font-bold text-4xl"><span class="border-b-4 border-black">Lin</span>gare</a>
+        </div>
+        <div class="flex items-center gap-8">
+          <nav class="hidden lg:flex gap-8">
+            <a href="" class="hover:underline">Collections</a>
+            <a href="" class="hover:underline">Featured products</a>
+            <a href="" class="hover:underline">Contact us</a>
+          </nav>
+          <span class="hidden lg:block">|</span>
+          <div class="flex gap-4 text items-center">
+            <a href="">
+              <i
+                class="flex items-center justify-center bg-black text-white rounded-full w-[35px] h-[35px] bi bi-search"></i>
+            </a>
+            <a href="">
+              <i
+                class="flex items-center justify-center bg-black text-white rounded-full w-[35px] h-[35px] bi bi-bag"></i>
+            </a>
+            <a href="" class="text-2xl">
+              <i
+                class="flex items-center justify-center bg-black text-white rounded-full w-[35px] h-[35px] bi bi-person"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+<style scoped>
+</style>
